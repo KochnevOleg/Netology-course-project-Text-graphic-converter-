5cdd423a1d850b7fcd27dc69420393a126d15180
KochnevOleg <95541637+KochnevOleg@users.noreply.github.com>
2022-03-31 22:50:25 +0300

    sqeeze convert method in TextColorSchemaClass, fix double for loop in convert method in Converter class

--- src/ru/netology/graphics/image/TextColorSchemaClass.java
+++ src/ru/netology/graphics/image/TextColorSchemaClass.java
@@ -2,29 +2,14 @@ package ru.netology.graphics.image;
 
 public class TextColorSchemaClass implements TextColorSchema{
 
-    private int color;
+    private final char [] characters;
+
+    public TextColorSchemaClass (char [] characters) {
+        this.characters = characters;
+    }
 
     @Override
     public char convert(int color) {
-        char character = 0;
-
-        if (color >= 0 && color <= 32) {
-            character = '#';
-        } else if (color > 32 && color <= 64){
-            character = '$';
-        } else if (color > 64 && color <= 96) {
-            character = '@';
-        } else if (color > 96 && color <= 128) {
-            character = '%';
-        } else if (color > 128 && color <= 160) {
-            character = '*';
-        } else if (color > 160 && color <= 192) {
-            character = '+';
-        } else if (color > 192 && color <= 224) {
-            character = '-';
-        }else if (color >224 && color <= 255) {
-            character = '/';
-        }
-        return character;
+        return characters[(int) Math.round((double)color / 32)];
     }
 }
